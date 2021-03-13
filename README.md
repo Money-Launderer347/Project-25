94eeb08d1d18effe8589a8c7e27a91316dd45c5b
Money-Launderer347 <77928588+Money-Launderer347@users.noreply.github.com>
2021-03-13 14:29:15 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Project-25
-Trash Game to encourage throwing your trash.
+# CP-2
---
+++ paper.js
@@ -0,0 +1,9 @@
+class paper extends basePaper{
+    constructor(x,y){
+        super(x,y,100,100);
+        this.image = loadImage("sprites/paper.png");
+    }
+    display(){
+        super.display();
+    }
+}
